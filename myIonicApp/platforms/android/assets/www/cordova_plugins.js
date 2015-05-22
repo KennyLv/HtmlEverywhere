7c4bad2beb4f198468126b7d8bac1bf66a73c8c9
Qing, Lv <luqin@airbiquity.com>
2015-05-22 10:36:47 +0800

    add htmlLayout and node-webkit and ionicapp demo

---
+++ myIonicApp/platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,26 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/com.ionic.keyboard/www/keyboard.js",
+        "id": "com.ionic.keyboard.keyboard",
+        "clobbers": [
+            "cordova.plugins.Keyboard"
+        ]
+    },
+    {
+        "file": "plugins/com.mbppower.camerapreview/www/CameraPreview.js",
+        "id": "com.mbppower.camerapreview.CameraPreview",
+        "clobbers": [
+            "cordova.plugins.camerapreview"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "com.ionic.keyboard": "1.0.4",
+    "org.apache.cordova.console": "0.2.13",
+    "com.mbppower.camerapreview": "0.0.8"
+}
+// BOTTOM OF METADATA
+});
